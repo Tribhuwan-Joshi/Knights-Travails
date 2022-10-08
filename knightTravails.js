57fd41cd99acaa9e5f50f1bec7593b293f8b2cf8
Tribhuwan <mathincosmos@gmail.com>
2022-10-08 22:42:44 +0530

    refactor into module pattern

--- knightTravails.js
+++ knightTravails.js
@@ -1,7 +1,3 @@
-function isValid(node) {
-  if (node.x >= 0 && node.x < 8 && node.y >= 0 && node.y < 8) return true;
-  return false;
-}
 class coords {
   constructor(x = 0, y = 0, dist = 0, prev = null) {
     this.x = x;
@@ -11,65 +7,79 @@ class coords {
   }
 }
 
-function knightMoves(start, end) {
-  if(! isValid(start) || ! isValid(end)) return "Provide valid Inputs"
-  let changes = [
-    [-1, -2],
-    [-2, -1],
-    [-2, 1],
-    [-1, 2],
-    [1, -2],
-    [2, -1],
-    [2, 1],
-    [1, 2],
-  ];
-  let visited = new Array(8).fill().map(() => Array(8).fill(0));
-  let queue = [new coords(start.x, start.y)];
-  visited[start.x][start.y] = 1;
-  while (queue.length) {
-    let node = queue[0];
-
-    if (node.x == end.x && node.y == end.y) {
-      return node;
-    }
-
-    for (let change of changes) {
-      let child = new coords(
-        change[0] + node.x,
-        change[1] + node.y,
-        node.dist + 1,
-        node
-      );
-
-      if (child.x == end[0] && child.y == end[1]) {
-        return child;
-      }
-
-      if (isValid(child) && !visited[child.x][child.y]) {
-        queue.push(child);
-
-        visited[child.x][child.y] = 1;
-      }
-    }
-    queue.shift();
+const KnightTravails = (() => {
+  function isValid(node) {
+    if (node.x >= 0 && node.x < 8 && node.y >= 0 && node.y < 8) return true;
+    return false;
   }
-}
 
-let ans= []
-function printPath(lastNode) {
-  if (lastNode == null) return ;
-  printPath(lastNode.prev);
-  let res = [lastNode.x , lastNode.y];
-  ans.push(res);
+  const knightMoves = (start, end) => {
+    if (!isValid(start) || !isValid(end)) return "Provide valid Inputs";
+    let changes = [
+      [-1, -2],
+      [-2, -1],
+      [-2, 1],
+      [-1, 2],
+      [1, -2],
+      [2, -1],
+      [2, 1],
+      [1, 2],
+    ];
+    let visited = new Array(8).fill().map(() => Array(8).fill(0));
+    let queue = [new coords(start.x, start.y)];
+    visited[start.x][start.y] = 1;
+    while (queue.length) {
+      let node = queue[0];
 
-}
-let startPositionX = 0;
-let startPositionY = 0;
-let endPositionX = 2;
-let endPositionY = 2;
-let startNode = new coords(startPositionX, startPositionY);
-let endNode = new coords(endPositionX, endPositionY);
-let lastNode = knightMoves(startNode,endNode);
-console.log("Minimum moves", lastNode.dist);
-printPath(lastNode);
-console.log("Full Path",ans);
+      if (node.x == end.x && node.y == end.y) {
+        return node;
+      }
+
+      for (let change of changes) {
+        let child = new coords(
+          change[0] + node.x,
+          change[1] + node.y,
+          node.dist + 1,
+          node
+        );
+
+        if (child.x == end[0] && child.y == end[1]) {
+          return child;
+        }
+
+        if (isValid(child) && !visited[child.x][child.y]) {
+          queue.push(child);
+
+          visited[child.x][child.y] = 1;
+        }
+      }
+      queue.shift();
+    }
+  };
+  return { isValid, knightMoves };
+})();
+
+
+
+const driver = (() => {
+  let ans = [];
+  function printPath(lastNode) {
+    if (lastNode == null) return;
+    printPath(lastNode.prev);
+    let res = [lastNode.x, lastNode.y];
+    ans.push(res);
+  }
+  // provide starting and ending coordinates
+
+  let startPositionX = 0;
+  let startPositionY = 6;
+  let endPositionX = 6;
+  let endPositionY = 7;
+
+  let startNode = new coords(startPositionX, startPositionY);
+  let endNode = new coords(endPositionX, endPositionY);
+  let lastNode = KnightTravails.knightMoves(startNode, endNode);
+  console.log("Minimum moves", lastNode.dist);
+  printPath(lastNode);
+  console.log("Full Path", ans);
+})();
